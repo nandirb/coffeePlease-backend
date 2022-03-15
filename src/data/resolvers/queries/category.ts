cf767c0d9526634e6630eb2215e5f91d0101aa4f
Orgil <49300787+khubster@users.noreply.github.com>
2022-03-15 19:26:34 +0800

    fixes

--- src/data/resolvers/queries/category.ts
+++ src/data/resolvers/queries/category.ts
@@ -4,8 +4,7 @@ const categoryQueries = {
   /**
    * Get category by list
    */
-  async categories(_root, { type }: { type: string }) {
-    console.log(type);
+  async categories(_root: { type: string }) {
     return await Categories.find();
   },
 };
